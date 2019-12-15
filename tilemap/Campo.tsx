a46b28781fdcee3d2cfd2f4e82f30b4c5eadbee7
Edws <eduardos.rocha17@gmail.com>
2019-12-15 15:01:00 -0300

    Imagens e fases
    musança no sprite do campo que passou a ser um spritesheet
    Adição de tilemap para primeira fase

---
+++ tilemap/Campo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Campo" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../images/campo.png" width="32" height="64"/>
+</tileset>
